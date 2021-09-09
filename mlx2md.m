1afb103305b8688a9526602296cb59210b58314d
SomefunAgba <oasomefun@futa.edu.ng>
2021-09-09 11:25:05 -0700

    clean: commit

--- mlx2md.m
+++
@@ -1,6 +0,0 @@
-% install mtlbx from addons directly
-% - eval(webread('https://git.io/JYcyl'))
-
-% convert
-livescript2markdown('README.mlx', 'README.md', 'AddMention', false);
-livescript2markdown('TEST.mlx', 'TEST.md', 'AddMention', false);
